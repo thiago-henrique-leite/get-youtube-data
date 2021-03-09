62eb7d7913a0c949cf383d83a4dc8a1cdc2b944c
Thiago <thiago.leitesilva05@gmail.com>
2021-03-08 21:07:23 -0300

    add colors

--- get_youtube_video_data/youtube.sh
+++ get_youtube_video_data/youtube.sh
@@ -2,13 +2,19 @@
 
 # author: Thiago Henrique Leite <thiago.leite@unifesp.com>
 # description: Get video and channel data on youtube by video url
-# version 1.0
+# version 2.0
 
 function youtube() {
+
+        _bl="\e[34;1m"
+        _gr="\e[32;1m"
+        _red="\e[31;1m"
+        _of="\e[m"
+
         local _video=$(mktemp)
         local _channel=$(mktemp)
 
-        wget $1 -O "$_video" &> /dev/null
+        wget "$1" -O "$_video" &> /dev/null
 
         local _videoTitle=$(grep '<title>' "$_video" | sed -n 1p | sed 's/.*<meta name="title" content="//g' | sed 's/".*//g')
         local _views=$(grep 'videoViewCountRenderer' "$_video" | sed -n 1p | sed 's/<[^>]*>//g;s/ visualizações.*//g;s/.*"//g')
@@ -18,21 +24,22 @@ function youtube() {
 
         local _url="https://youtube.com/channel"
         local _channelID=$(sed -n '/channelId/{p; q;}' "$_video" | sed 's/"videoId.*//g;s/.*content="//g;s/".*//g')
-        
         wget "$_url/$_channelID" -O "$_channel" &> /dev/null
 
         local _channelTitle=$(grep '<title>' "$_channel" | sed 's/<\/title><meta name="description".*//g;s/.*<title>//g;s/ - YouTube.*//g')
         local _subscribers=$(grep 'subscriberCount' "$_channel" | sed 's/.*subscriberCountText":{"simpleText":"//g;s/".*//g')
 
         echo ""
-        echo "Canal: $_channelTitle"
-        echo "Inscritos: $_subscribers"
+        echo -e "$_red    ############# YouTube #############"
         echo ""
-        echo "Vídeo: $_videoTitle"
-        echo "Visualizações: $_views"
-        echo "Likes: $_likes"
-        echo "Dislikes: $_dislikes"
-        echo "Data de publicação: $_postDate"
+        echo -e "$_bl Canal:$_gr $_channelTitle"
+        echo -e "$_bl Inscritos:$_gr $_subscribers"
+        echo ""
+        echo -e "$_bl Vídeo:$_gr $_videoTitle"
+        echo -e "$_bl Visualizações:$_gr $_views"
+        echo -e "$_bl Likes:$_gr $_likes"
+        echo -e "$_bl Dislikes:$_gr $_dislikes"
+        echo -e "$_bl Data de publicação:$_gr $_postDate $_of"
         echo ""
 }
 
